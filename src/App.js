c989dcbf7d9a5998391d07a2e1bbbf451192b9a1
Daryl Magera <darylmagera@gmail.com>
2019-07-11 19:56:15 -0400

    Final commit day two

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios'
-import "./App.css";
 import DateForm from "./components/DateForm";
 import PhotoDisplay from "./components/PhotoDisplay";
+import { WrapperApp } from "./components/Styles";
+import { HeadingH1 } from "./components/Styles";
 
 
 
@@ -11,34 +12,34 @@ function App() {
 const [data, setData] = useState([]);
 const [date, setDate] = useState('');
 
+
 useEffect( ()=> {
 const fetchData = () => {
   axios
-    .get(`https://api.nasa.gov/planetary/apod?api_key=6KVg1jJIEWTFQTcFIaa47acZm2o6WFNuup3W7ij0&date=${date}`)
+    .get(`https://api.nasa.gov/planetary/apod?api_key=DNHHO0zcS1YFNJ005eFCq56wy4UzlDWYPp6QaBu7&date=${date}`)
     .then( res => {
       setData(res.data)
-      console.log(res)
+      // console.log(res)
     })
     .catch( err => console.log('Fetch error: ',err))
 
   }
 
-  fetchData()
+  fetchData();
 }, [date])
 
 let changeHandler = e => {
   setDate(e.target.value)
 }
 
-
   return (
-    <div className="App">
-      <h1>
+    <WrapperApp>
+      <HeadingH1>
         Nasa Photo of The Day!🚀
-      </h1>
+      </HeadingH1>
       <DateForm  date={date} change={changeHandler}/>
       <PhotoDisplay photo={data} />
-    </div>
+    </WrapperApp>
   );
 }
 
